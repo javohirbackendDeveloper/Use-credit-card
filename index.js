b90ffc225973c40c82446d7751ab685be8eb58f9
javohirfrontenddevs <javohirabdusharipov676@gmail.com>
2024-11-04 23:00:12 +0500

    added package.json

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let card = 0; // Global o'zgaruvchi, mablag'ni saqlaydi
+let card = 0; 
 
 function work() {
   let addMoney = parseFloat(document.getElementById("addMoney").value);
   
-  if (addMoney > 0) { // Iltimos summani kiritish
-    card += addMoney; // Kartadagi mablag'ni yangilash
+  if (addMoney > 0) { 
+    card += addMoney; 
     document.getElementById('output').innerHTML = "Mablag' muvaffaqiyatli qo'shildi";
-    document.getElementById('outputCard').innerHTML = card; // Yangilangan mablag'ni chiqarish
+    document.getElementById('outputCard').innerHTML = card; 
   } else {
     document.getElementById('output').innerHTML = "Iltimos, to'g'ri summani kiriting";
   }
@@ -15,11 +15,11 @@ function work() {
 function work2() {
   let removeMoney = parseFloat(document.getElementById("removeMoney").value);
   
-  if (removeMoney > 0) { // Iltimos summani kiritish
-    if (removeMoney <= card) { // Mablag' yechish
-      card -= removeMoney; // Kartadagi mablag'ni yangilash
+  if (removeMoney > 0) { 
+    if (removeMoney <= card) { 
+      card -= removeMoney; 
       document.getElementById('output').innerHTML = "Mablag' muvaffaqiyatli yechildi";
-      document.getElementById('outputCard').innerHTML = card; // Yangilangan mablag'ni chiqarish
+      document.getElementById('outputCard').innerHTML = card; 
     } else {
       document.getElementById('output').innerHTML = "Kechirasiz, sizning hisobingizda mablag' yetarli emas";
     }
